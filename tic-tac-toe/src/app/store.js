51b33ebfb526ca28d3baa8e1c05f26c7240917dc
Mathieu Desjarlais <github@desmat.ca>
2021-12-14 21:10:12 -0500

    working app

--- tic-tac-toe/src/app/store.js
+++ tic-tac-toe/src/app/store.js
@@ -1,8 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit'
+import gameReducer from '../features/tic-tac-toe/gameSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    game: gameReducer,
   },
-});
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // // Ignore these field paths in all actions
+        ignoredActionPaths: ['payload.onWin', 'payload.onDraw'],
+      },
+    }),  
+})
